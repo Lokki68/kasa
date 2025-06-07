06c6f717789261df26d7337d0a1ab44294b26c21
Lokki <bricel67@gmail.com>
2025-06-07 17:07:34 +0200

    Not found page

--- src/Pages/NotFound/index.jsx
+++ src/Pages/NotFound/index.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import {NavLink} from "react-router-dom";
+import Styles from './not_found.module.scss'
 
 const NotFound = () => {
     return (
-        <div>
-            Not Found Page
+        <div className={Styles.not_found} >
+            <div className={Styles.content}>
+                <h2>404</h2>
+                <p>Oups! La page que vous demandez n'existe pas.</p>
+            </div>
+            <NavLink to='/'>
+                Retourner sur la page d'accueil
+            </NavLink>
         </div>
     );
 };
