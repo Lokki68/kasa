42f46c209c2f5cf43241b847adc46e13f62832ed
Lokki <bricel67@gmail.com>
2025-06-08 16:30:04 +0200

    Carrousel Component

--- src/Components/Logement/Carrousel/index.jsx
+++ src/Components/Logement/Carrousel/index.jsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import Styles from './carrousel.module.scss'
+import {useState} from "react";
+import {faChevronLeft, faChevronRight} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Carrousel = ({pictures}) => {
+    const picturesLength = pictures.length
+    const [carrouselIndex, setCarrouselIndex] = useState(0)
+
+    const handlePreviousPicture = () => {
+        if (carrouselIndex > 0) {
+            setCarrouselIndex(carrouselIndex - 1)
+        } else {
+            setCarrouselIndex(picturesLength - 1)
+        }
+    }
+
+    const handleNextPicture = () => {
+        if (carrouselIndex < picturesLength - 1) {
+            setCarrouselIndex(carrouselIndex + 1)
+        } else {
+            setCarrouselIndex(0)
+        }
+    }
+
     return (
-        <div>
-            Carrousel
+        <div className={Styles.carrouselContainer} >
+            <div className={Styles.actionsContainer} >
+                <FontAwesomeIcon
+                    icon={faChevronLeft}
+                    onClick={handlePreviousPicture}
+                />
+                <FontAwesomeIcon
+                    icon={faChevronRight}
+                    onClick={handleNextPicture}
+                />
+            </div>
+
+            <div className={Styles.numberContainer} >
+                <p>{carrouselIndex +1} / {picturesLength}</p>
+            </div>
+
+            <div className={Styles.carrouselPicture} >
+                <img
+                    src={pictures[carrouselIndex]}
+                    alt={`picture ${carrouselIndex}`}
+                    loading="lazy"
+                />
+            </div>
         </div>
     );
 };
