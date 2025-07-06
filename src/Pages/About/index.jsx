ead2dfeb2a54d39a4edaba4d2c97c2e5910cd16c
LIBERT Brice <bricel67@gmail.com>
2025-07-06 10:37:03 +0200

    fix all responsive bug

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -2,18 +2,20 @@ import about_picture from '../../assets/about_picture.png'
 
 import Banner from "../../Components/Banner/index.jsx";
 import Collapse from "../../Components/Collapse/index.jsx";
-
+import styles from './about.module.scss'
 import {aboutData} from "../../Data/about_data.js";
 
 const About = () => {
     return (
         <div>
             <Banner picture={about_picture} height />
-            {
-                aboutData.map((item, index) => (
-                    <Collapse key={index} title={item.title} content={item.content} open={index === 0} />
-                ))
-            }
+            <div className={styles.collapse_container} >
+                {
+                    aboutData.map((item, index) => (
+                        <Collapse key={index} title={item.title} content={item.content} open={index === 0} />
+                    ))
+                }
+            </div>
         </div>
     );
 };
